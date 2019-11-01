cdee09208c278cd49ea23921ae8145190f95b4fe
Asher Noel <asher13a@gmail.com>
2019-11-01 00:22:20 -0400

    Upgraded trend scraper

--- analyze.py
+++ analyze.py
@@ -2,8 +2,13 @@
 # Connect Pytrends API to google
 from pytrends.request import TrendReq
 import pandas as pd
+import numpy as np
+import string
 
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from wordfreq import zipf_frequency
+
 
 # Remove the old hyper files from the last run time. Comment
 # Connect with Google somehow
@@ -18,9 +23,9 @@ college_colors = {"Harvard University": "#A51C30",
                   "Brown University": "#4E3629",
                   "Columbia University": "#B9D9EB",
                   "Dartmouth College": "#00693e",
-                  "University of Pennsylvania": "tab:purple"
-                  }
+                  "University of Pennsylvania": "tab:purple",
 
+                  }
 
 def main():
 
@@ -29,21 +34,37 @@ def main():
                       'Massachusetts Institute of Technology', 'Princeton University',
                       'Yale University']
 
-    colleges_ivyLeague = ["Harvard University", "Brown University", "Columbia University",
-                          "Dartmouth College", "University of Pennsylvania", "Princeton University",
-                          "Yale University", "Cornell University"]
+    harvard = ["Netflix"]
 
-    ivyUS = getTrendsData(colleges_ivyLeague, 'US', 'today 5-y', True)
+    # region = ''
+    # timeframe = 'now 7-d'
+    # #
+    # pytrends.build_payload([pytrends.suggestions("harvard")[0]['mid']], cat=0, timeframe=timeframe, geo=region, gprop='')
+    # # newData = pytrends.related_topics()
+    #
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    #     print(getRisingRelatedTopics("Netflix", "", timeframe, 250))
+    # #
+    # print(len(getTimeframes("2018-12-13", "2019-11-14", 7)))
 
+    netflixMegaData = getMegaTrendData("Netflix", '', '2019-02-03 2019-10-30', 30, 600)
 
-    # Rename the columns so they in english again in the same order as the original list
-    ivyUSOut = getAnnualAverages(ivyUS, 5, 2015)
+    plotStackedHist(netflixMegaData, "Netflix Mega Data", False)
+    plotStackedHist(netflixMegaData.iloc[:, 1:], "Netflix Data without Netflix", False)
 
-    print(ivyUSOut.mean())
+    # colleges_ivyLeague = ["Harvard University", "Brown University", "Columbia University",
+    #                       "Dartmouth College", "University of Pennsylvania", "Princeton University",
+    #                       "Yale University", "Cornell University", "Northeastern University"]
+    #
+    # ivyUS = getTrendsData(colleges_ivyLeague, 'US', 'today 5-y', True)
+    #
+    # # Rename the columns so they in english again in the same order as the original list
+    # ivyUSOut = getAnnualAverages(ivyUS, 5, 2015)
+    #
+    #
+    # plotLine(ivyUSOut.iloc[:, ::-1], "Domestic Ivy League Search Data Over Time")
+    # plotStackedHist(ivyUSOut.iloc[:, ::-1], "Domestic Ivy League Search Data Over Time")
 
-    plotLine(ivyUSOut.iloc[:, ::-1], "Domestic Ivy League Search Data Over Time")
-
-    plotStackedHist(ivyUSOut.iloc[:, ::-1], "Domestic Ivy League Search Data Over Time")
 
 # Get the annual averages from the master data DataFrame
 def getAnnualAverages(data, numYears, startingYear):
@@ -59,10 +80,12 @@ def getAnnualAverages(data, numYears, startingYear):
     output = output.div(output.sum(axis=1), axis=0)
     return output
 
+
 def getTrendsData(keywords, region, timeframe, flagTopic):
 
     # FLAG: If FLAG = FALSE, keywords will be "Search Terms"
     #       If FLAG = TRUE, keywords will be the first "Topic" that comes up.
+    # NOTE: keywords can NOT have duplicates!
 
     # Initialize flag and temporary list of quieries with the keyword that should be in all of them.
     old = 0
@@ -77,25 +100,41 @@ def getTrendsData(keywords, region, timeframe, flagTopic):
         # Add the next keyword in the list to the temporary list of queries
         keyword = keywords[index]
 
-        tempList.append(pytrends.suggestions(keyword)[0]['mid']) if flagTopic else tempList.append(keyword)
+        # If the keyword does not have enough traffic, it will not be a topic; in these cases, add it as a search term.
+        try:
+            tempList.append(pytrends.suggestions(keyword)[0]['mid']) if flagTopic else tempList.append(keyword)
+        except IndexError:
+            tempList.append(keyword)
 
         # The maximum query request is five terms
         if index // 4 != old or (index == len(keywords) - 1):
 
             # Build the payload
             pytrends.build_payload(tempList, cat=0, timeframe=timeframe, geo=region, gprop='')
+
             newData = pytrends.interest_over_time()
+
+
+            #Adjust the data so that the first column, the master column, has a maximum of 100
+            maxMaster = newData[pytrends.suggestions(keywords[0])[0]['mid']].max() if flagTopic else newData[keywords[0]].max()
+
+            newData.iloc[:, :-1] *= 100/(maxMaster)
 
             # Merge the old and new DataFrames
             if old == 0:
                 oldData = newData.iloc[:, :-1]
                 trendData = oldData
             else:
-                trendData = oldData.join(newData.iloc[:, 1:-1])
+                try:
+                    trendData = oldData.join(newData.iloc[:, 1:-1])
+                except ValueError:
+                    trendData = oldData.join(newData.iloc[:, 1:-1], lsuffix = '_left', rsuffix = '_right')
+                oldData = trendData
 
             # Reset the temporary queries and increase the flag.
             tempList = [pytrends.suggestions(keywords[0])[0]['mid']] if flagTopic else [keywords[0]]
             old += 1
+
 
     # Update the columns of the trend data with the original names
     trendData.columns = keywords
@@ -103,19 +142,142 @@ def getTrendsData(keywords, region, timeframe, flagTopic):
     # Reorder the columns based off of their mean values.
     return trendData.reindex(trendData.mean().sort_values(ascending=False).index, axis=1)
 
-def plotStackedHist(data, title):
+
+def getRisingRelatedTopics(keyword, region, timeframe, cutoff):
+    # Inputs:
+        # keyword is a string format
+        # region: "" for world, "US" for US
+        # timeframe: YYYY-MM-DD YYYY-MM-DD format
+        # cutoff: INT the % minimum % increase in searches over the period to be interesting (e.g., 100% = 100)
+
+    pytrends.build_payload([pytrends.suggestions(keyword)[0]['mid']], cat=0, timeframe=timeframe, geo=region,
+                           gprop='')
+    topics = pytrends.related_topics()
+
+
+    #Only return the topics above the cutoff
+    return topics[pytrends.suggestions(keyword)[0]['mid']]['rising'].loc[topics[pytrends.suggestions(keyword)[0]['mid']]['rising']['value'] >= cutoff]
+
+def getMegaTrendData(keyword, region, timeframe, interval, cutoff):
+    # INPUT: Timeframe in YYYY-MM-DD YYYY-MM-DD format
+
+    keywords = [keyword]
+
+    # Track the type of related topic
+    keyword_types = []
+
+    # Track the number of rising topics per time interval over time.
+    virality = []
+
+
+    # Add all of the related topics over that timeframe to the keywords list
+    times = getTimeframes(timeframe[:10], timeframe[11:], interval)
+    for time in times:
+        topics = getRisingRelatedTopics(keyword, region, time, cutoff)
+        keyword_types.extend(topics['topic_type'].tolist())
+        keywords.extend(topics['topic_title'].tolist())
+
+        virality.append(len(topics['topic_title'].tolist()))
+
+    plotLine(virality, "Virality over time")
+    print(keyword_types)
+
+    # Remove duplicates and punctuation from keywords:
+    adjKeywords = []
+    [adjKeywords.append(word.translate(str.maketrans('', '', string.punctuation))) for word in keywords if (word.translate(str.maketrans('', '', string.punctuation)) not in adjKeywords) and len(word) > 2]
+
+    # Remove single words that are too common in english like Mother or July
+    adj2Keywords = []
+    [adj2Keywords.append(word) for word in adjKeywords if not (" " not in word and zipf_frequency(word, 'en') > 4.4)]
+
+    # Remove words with the same topics.
+    topics = []
+    adj3Keywords = []
+    for word in adj2Keywords:
+        try:
+            if pytrends.suggestions(word)[0]['mid'] not in topics:
+                adj3Keywords.append(word)
+                topics.append(pytrends.suggestions(word)[0]['mid'])
+        except IndexError:
+            continue
+
+    print(f"Removed common words:       {list(set(adjKeywords) - set(adj2Keywords))}")
+    print(f"Removed repeat topic words: {list(set(adjKeywords) - set(adj3Keywords))}")
+    print(adj3Keywords)
+    print(len(adj3Keywords))
+    # Get the interest over time.
+    return getTrendsData(adj3Keywords, region, timeframe, True)
+
+def getTimeframes(start, end, interval):
+    #start and end are in YYYY-MM-DD format.
+    #interval is an integer in days.
+    #return a list of all of the intervals in [YYYY-MM-DD YYYY-MM-DD] between a start and end date
+
+    intervals = []
+
+    startYear = int(start[:4]); startMonth = int(start[5:7]); startDay = int(start[8:])
+
+    while decimalTime(start) < decimalTime(end):
+        tempDay = startDay + interval; tempYear = startYear; tempMonth = startMonth
+
+        #Adjust the day and month
+        if tempDay > 30 and startMonth != 2:
+            tempDay = tempDay % 30
+            tempMonth += 1
+        elif tempDay > 28 and startMonth == 2:
+            tempDay = tempDay % 28
+            tempMonth += 1
+
+        #Adjust the month and year
+        if tempMonth > 12:
+            tempYear += 1
+            tempMonth = tempMonth % 12
+
+        #add the new interval to intervals
+        start = toDatetime(tempYear, tempMonth, tempDay)
+
+        intervals.append(toDatetime(startYear, startMonth, startDay) + " " + start)
+
+        startDay = tempDay; startMonth = tempMonth; startYear = tempYear
+
+    return intervals
+
+
+def decimalTime(time):
+    #input in YYYY-MM-DD format
+    #output in YYYY.XX format.
+    return int(time[:4]) + int(time[5:7])/12 + int(time[8:])/365
+
+
+def toDatetime(year, month, day):
+    #input as integers, output as YYYY-MM-DD
+    MM = "0" + str(month) if month < 10 else str(month)
+    return str(year) + "-" + str(MM) + "-" + str(day)
+
+
+def plotStackedHist(data, title, legendFlag):
+    # INPUT: if legendFlag is TRUE: then REVERSE the legend
+    #        else, write it normally
+
     plt.figure()
 
-    # Get the colors for the colleges
-    colors = []
-    for column in data.columns:
-        colors.append(college_colors[column])
+
+    # Get the colors for the data
+    colors = getColors(data)
 
     # Plot the data
     data.plot.bar(stacked=True, color=colors, figsize=(10,7))
 
+    # Choose a font size
+    fontP = FontProperties()
+    fontP.set_size('small')
+
     # Adjust and show the rest of the figure
-    plt.legend(reversed(plt.legend().legendHandles), reversed(data.columns), loc='upper right')
+    if legendFlag:
+        plt.legend(reversed(plt.legend().legendHandles), reversed(data.columns), loc='upper right', prop=fontP)
+    else:
+        plt.legend(data.columns, loc='upper right', prop=fontP)
+
     plt.title(title)
     plt.xticks(rotation='horizontal')
     plt.show()
@@ -124,10 +286,8 @@ def plotStackedHist(data, title):
 def plotLine(data, title):
     plt.figure()
 
-    # Get the colors for the colleges
-    colors = []
-    for column in data.columns:
-        colors.append(college_colors[column])
+    # Get the colors for the data
+    colors = getColors(data)
 
     # Plot the data
     data.plot.line(color=colors)
@@ -137,6 +297,19 @@ def plotLine(data, title):
     plt.title(title)
     plt.xticks(rotation='horizontal')
     plt.show()
+
+
+def getColors(data):
+    colors = []
+    for column in data.columns:
+        if column in college_colors:
+            colors.append(college_colors[column])
+
+    # Randomly pick colors generally for when there are not colleges
+    while len(colors) < len(data.columns):
+        colors.insert(0, np.random.rand(3, ))
+
+    return colors
 
 main()
 
